3992d535804cbc532ca2f4f298750a026a5a2701
Jye <jyesawtellrickson@gmail.com>
2017-04-23 15:52:15 +0800

    Update sequences.py

--- Number_Sequences/models/sequences.py
+++ Number_Sequences/models/sequences.py
@@ -2,10 +2,13 @@ import pandas as pd
 import numpy as np
 from sklearn import linear_model
 
-test_df = pd.read_csv('input/test.csv')
-seq_df = pd.read_csv('input/train.csv')
-
 def seq_to_list(str_seq):
+    """
+    Convert string sequence to list.
+
+    :param str_seq: sequence string
+    :return: sequence list of ints
+    """
     str_seql = str_seq.split(',')
     num_seql = [int(i) for i in str_seql]
     return num_seql
@@ -22,20 +25,19 @@ for seq in targets_list:
 #print(targets_df.head())
 """
 
-# How do we do this?
-"""
-    Maybe we can run a linear model which predicts the next number based off the previous few.
 
-    This would work for number 2 in an n-gram approach.
-
-    Not all sequences are linear though.
-
-"""
-
-"""
-    Takes a sequence a creates a range of values for use in ML problems.
-"""
 def seq_to_data(seq, n=4):
+    """
+    Takes a sequence and creates a training/test set.
+
+    Takes a sequence as input and generates many
+    training examples of varying length to support
+    the model 'learning' the series.
+
+    :param seq: sequence list
+    :param n: size of the desired training set int
+    :return:
+    """
     X = []
     y = []
     for i in range(0, len(seq)-n-1):
@@ -48,7 +50,16 @@ def seq_to_data(seq, n=4):
 
     return X, y, X_p
 
+
 def lin_pred(seq, print_= 0):
+    """
+    Perform a linear prediction to get the next
+    number in the sequence.
+
+    :param seq: a numerical sequence list
+    :param print_: flag for enabling printing
+    :return: predcition, int
+    """
     # get data
     if len(seq) <= 3:
         return 1, seq[-1:][0]
@@ -57,8 +68,8 @@ def lin_pred(seq, print_= 0):
 
     # Train a linear model
     lmr = linear_model.LinearRegression()
-
     lmr.fit(X, y)
+    # predict next number
     seq_pred = int(round(lmr.predict(X_p)[0],0))
     seq_ans = seq[-1:][0]
     if print_ == 1:
@@ -69,27 +80,27 @@ def lin_pred(seq, print_= 0):
 
     return seq_pred, seq_ans
 
-#seq_pred, seq_ans = lin_pred(seq_2, 0)
+# Load data
+test_df = pd.read_csv('../input/test.csv')
+seq_df = pd.read_csv('../input/train.csv')
+
 
 seq_preds = []
 seq_anss = []
-for i in range(0,seq_df.shape[0]):
+# iterate through each sequence and generate a model
+for i in range(0, seq_df.shape[0]):
     seq = seq_to_list(seq_df['Sequence'][i])
     seq_pred, seq_ans = lin_pred(seq, 0)
     seq_preds += [seq_pred]
     seq_anss += [seq_ans]
+    # let user know progress
     if i % 1000 == 0:
-        print(i)
-
-#print(seq_preds)
-#print(seq_anss)
+        print("{0:.0f}%".format(100*i/seq_df.shape[0]))
 
 corr = sum([seq_preds[i]==seq_anss[i] for i in range(0,len(seq_preds))])
 
 print("Correct: ", corr, " out of ", len(seq_preds))
 
-# Can't use basic regression, least squares doesn't accurately hone in. Need to use
-# more of a classification style
 """
 def my_score(X,y):
     cor = 0
@@ -97,4 +108,4 @@ def my_score(X,y):
         pred_ = int(round(lmr.predict(X[i])[0],0))
         if pred_ == y[i]:
             cor += 1
-            """
+"""
