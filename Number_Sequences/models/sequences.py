a5c23f82edd749049f5a0802eb7b4c142913d8ad
Jye <jyesawtellrickson@gmail.com>
2017-04-23 16:46:08 +0800

    Update Number_Sequences.

--- Number_Sequences/models/sequences.py
+++ Number_Sequences/models/sequences.py
@@ -9,9 +9,8 @@ def seq_to_list(str_seq):
     :param str_seq: sequence string
     :return: sequence list of ints
     """
-    str_seql = str_seq.split(',')
-    num_seql = [int(i) for i in str_seql]
-    return num_seql
+    return [int(i) for i in str_seq.split(',')]
+
 
 """
 targets_list = seq_df['Sequence'].str.split(',').tolist()
@@ -31,11 +30,11 @@ def seq_to_data(seq, n=4):
     Takes a sequence and creates a training/test set.
 
     Takes a sequence as input and generates many
-    training examples of varying length to support
+    training examples of set length to support
     the model 'learning' the series.
 
     :param seq: sequence list
-    :param n: size of the desired training set int
+    :param n: min size of training set int
     :return:
     """
     X = []
@@ -43,7 +42,8 @@ def seq_to_data(seq, n=4):
     for i in range(0, len(seq)-n-1):
         X.append(seq[i:i+n])
         y.append(seq[i+n])
-
+    # create sequence for prediction
+    # this is the last n elements
     X_p = np.array(seq[-(n+1):-1])
     X = np.array(X)
     y = np.array(y)
@@ -60,25 +60,28 @@ def lin_pred(seq, print_= 0):
     :param print_: flag for enabling printing
     :return: predcition, int
     """
-    # get data
+    # if small sequence, just guess 1 ## why?
     if len(seq) <= 3:
         return 1, seq[-1:][0]
+    # choose size of sequence chunks
     fil_len = int(len(seq)/2)
+    # generate data for prediction
     X, y, X_p = seq_to_data(seq, fil_len)
-
-    # Train a linear model
+    # train a linear model
     lmr = linear_model.LinearRegression()
     lmr.fit(X, y)
-    # predict next number
-    seq_pred = int(round(lmr.predict(X_p)[0],0))
+    # predict next number, round for int
+    seq_pred = int(round(lmr.predict(X_p)[0], 0))
     seq_ans = seq[-1:][0]
+    # print results
     if print_ == 1:
         print('Sequence: ', seq)
         print('My prediction: ', seq_pred)
         print('Correct answer: ', seq_ans)
-        print('Predicted accuracy: ', lmr.score(X,y))
+        print('Predicted accuracy: ', lmr.score(X, y))
 
     return seq_pred, seq_ans
+
 
 # Load data
 test_df = pd.read_csv('../input/test.csv')
@@ -97,8 +100,8 @@ for i in range(0, seq_df.shape[0]):
     if i % 1000 == 0:
         print("{0:.0f}%".format(100*i/seq_df.shape[0]))
 
-corr = sum([seq_preds[i]==seq_anss[i] for i in range(0,len(seq_preds))])
-
+# count correct predictions
+corr = sum([seq_preds[i]==seq_anss[i] for i in range(0, len(seq_preds))])
 print("Correct: ", corr, " out of ", len(seq_preds))
 
 """
